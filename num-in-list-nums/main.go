7693b69ef357f9791ee57f5009f1ff3b8693ecfa
7kFour <48108297+7kFour@users.noreply.github.com>
2022-03-23 12:48:23 -0500

    update code main.go

---
+++ num-in-list-nums/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(numInList([]int{1, 2, 3, 4, 5}, 5))      // true
+	fmt.Println(numInList([]int{3, 3, 3, 3, 3}, 5))      // false
+	fmt.Println(numInList([]int{3, 5, 3, 5, 3}, 5))      // true
+	fmt.Println(numInList([]int{4, 2, 22, -10, 8}, -10)) // true
+
+	// testing for empty lists
+	fmt.Println(numInList(nil, 5))     // false
+	fmt.Println(numInList([]int{}, 5)) // false
+}
+
+// numInList will return true if num is in the list slice, and false otherwhise
+func numInList(list []int, num int) bool {
+	// think about what algo you want to use?
+	// if sorted list could use search which is faster
+	// lack of knowledge about the list means you must check every value
+	// could use a map for this - or check every number (which is slower but more simple)
+
+	for _, val := range list {
+		if val == num {
+			return true
+		}
+	}
+
+	return false
+}
+
+// just because you see some test cases pass don't assume your code is correct
